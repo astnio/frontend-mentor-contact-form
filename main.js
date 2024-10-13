3f56823fc5eadfd654db4a2e2de1d81cb0b443e4
astn <github@msg.austinh.io>
2024-10-13 15:46:04 -0700

    Adjusted input validation script

--- main.js
+++ main.js
@@ -1,44 +1,78 @@
-const inputs = document.querySelectorAll('input');
-const textareaInput = document.querySelector('textarea');
+const inputsText = document.querySelectorAll('input[type="text"');
+const inputEmail = document.querySelector('input[type="email"');
+const inputRadio = document.querySelector('input[type="radio"]');
+const inputTextarea = document.querySelector('textarea');
+const inputCheckbox = document.querySelector('input[type="checkbox"]');
 const btnSubmit = document.getElementById('btn-contact-form-submit');
 
 function isInputValid(inputElement) {
-  return inputElement.validity.valid === false;
+  return inputElement.validity.valid;
 }
 
-function handleInvalidInput(inputElement) {
-  let inputErrorWrapper = inputElement.parentElement.querySelector(
-    '.input-error-wrapper'
-  );
-
-  if (!inputErrorWrapper) {
-    inputErrorWrapper = inputElement.parentElement.parentElement.querySelector(
-      '.input-error-wrapper'
-    );
-  }
-
-  if (isInputValid(inputElement)) {
+function setInvalidMessageActive(inputElement, inputErrorWrapper) {
+  if (!isInputValid(inputElement)) {
     inputErrorWrapper.dataset.active = true;
   } else {
     inputErrorWrapper.dataset.active = false;
   }
 }
 
-function checkAllInputsValidity() {
-  inputs.forEach((inputElement) => {
-    handleInvalidInput(inputElement);
-  });
-  handleInvalidInput(textareaInput);
+function handleInvalidText(inputElement) {
+  const inputErrorWrapper = inputElement.parentElement.querySelector(
+    '.input-error-wrapper'
+  );
+
+  setInvalidMessageActive(inputElement, inputErrorWrapper);
 }
 
-inputs.forEach((inputElement) => {
-  inputElement.addEventListener('blur', () => {
-    handleInvalidInput(inputElement);
+function handleInvalidRadio(inputElement) {
+  const inputErrorWrapper =
+    inputElement.parentElement.parentElement.parentElement.querySelector(
+      '.input-error-wrapper'
+    );
+
+  setInvalidMessageActive(inputElement, inputErrorWrapper);
+}
+
+function handleInvalidCheckbox(inputElement) {
+  const inputErrorWrapper =
+    inputElement.parentElement.parentElement.querySelector(
+      '.input-error-wrapper'
+    );
+
+  setInvalidMessageActive(inputElement, inputErrorWrapper);
+}
+
+function checkAllInputsValidity() {
+  inputsText.forEach((inputElement) => {
+    handleInvalidText(inputElement);
   });
-});
+  handleInvalidText(inputEmail);
+  handleInvalidRadio(inputRadio);
+  handleInvalidText(inputTextarea);
+  handleInvalidCheckbox(inputCheckbox);
+}
 
-textareaInput.addEventListener('blur', () => {
-  handleInvalidInput(textareaInput);
-});
+function initInputEventListeners() {
+  inputsText.forEach((inputElement) => {
+    inputElement.addEventListener('blur', () => {
+      handleInvalidText(inputElement);
+    });
+  });
 
-btnSubmit.addEventListener('click', checkAllInputsValidity);
+  inputEmail.addEventListener('blur', () => handleInvalidText(inputEmail));
+
+  inputRadio.addEventListener('blur', () => handleInvalidRadio(inputRadio));
+
+  inputTextarea.addEventListener('blur', () => {
+    handleInvalidText(inputTextarea);
+  });
+
+  inputCheckbox.addEventListener('blur', () =>
+    handleInvalidCheckbox(inputCheckbox)
+  );
+
+  btnSubmit.addEventListener('click', checkAllInputsValidity);
+}
+
+initInputEventListeners();
