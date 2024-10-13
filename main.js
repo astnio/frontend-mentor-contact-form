0ec3637a6dde9ca1a98cd9b2ac2126f10f4e0020
astn <github@msg.austinh.io>
2024-10-13 13:05:41 -0700

    All inputs now show error if invalid

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 const inputs = document.querySelectorAll('input');
-const textarea = document.querySelector('textarea');
+const textareaInput = document.querySelector('textarea');
 const btnSubmit = document.getElementById('btn-contact-form-submit');
 
 function isInputValid(inputElement) {
-  return (
-    inputElement.validity.valid === false &&
-    inputElement.matches(':not(:focus):not(:placeholder-shown)')
-  );
+  return inputElement.validity.valid === false;
 }
 
 function handleInvalidInput(inputElement) {
-  const inputErrorWrapper = inputElement.parentElement.querySelector(
+  let inputErrorWrapper = inputElement.parentElement.querySelector(
     '.input-error-wrapper'
   );
+
+  if (!inputErrorWrapper) {
+    inputErrorWrapper = inputElement.parentElement.parentElement.querySelector(
+      '.input-error-wrapper'
+    );
+  }
+
   if (isInputValid(inputElement)) {
     inputErrorWrapper.dataset.active = true;
   } else {
@@ -24,35 +28,17 @@ function checkAllInputsValidity() {
   inputs.forEach((inputElement) => {
     handleInvalidInput(inputElement);
   });
-  handleInvalidInput(textarea);
+  handleInvalidInput(textareaInput);
 }
 
 inputs.forEach((inputElement) => {
-  console.log(inputElement);
   inputElement.addEventListener('blur', () => {
     handleInvalidInput(inputElement);
   });
 });
 
-textarea.addEventListener('blue', () => {
-  handleInvalidInput(textarea);
+textareaInput.addEventListener('blue', () => {
+  handleInvalidInput(textareaInput);
 });
 
 btnSubmit.addEventListener('click', checkAllInputsValidity);
-
-// buttons.forEach((btn) => {
-//   btn.addEventListener('click', (e) => {
-//     const paragraph = e.target.parentElement.querySelector(
-//       '.card-paragraph-wrapper'
-//     );
-//     const images = e.target.parentElement.querySelectorAll('img');
-
-//     images.forEach((img) => {
-//       let imgActive = img.dataset.active === 'true';
-//       img.dataset.active = (!imgActive).toString();
-//     });
-
-//     let active = paragraph.dataset.active === 'true';
-//     paragraph.dataset.active = (!active).toString();
-//   });
-// });
